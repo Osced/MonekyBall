133a5a63a0d581a1198b834b5e4d453d8130c45b
Johnny Chip <johngomez1115@gmail.com>
2017-05-24 23:23:17 -0500

    Initial Commit

---
+++ Ch6_MonkeyBall_CS/Assets/Scripts/SpeedBall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedBall : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+
+		if (Input.GetMouseButtonDown (0)) 
+		{
+
+			rb.AddRelativeForce (Vector3.forward *1000f);
+
+		}
+		
+	}
+}
